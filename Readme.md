b802f37470aeda49fc5e1bfbc92c6c9f2dbfe523
Corentin <corentin.lger@gmail.com>
2023-11-13 12:16:44 +0100

    Add multiple clients

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-Minimal example to test how to connect to a server from a jupyter notebook, and change elements of a simulation environment 
+This minimal example demonstrates connecting to a server from multiple Jupyter notebooks to interact with a simulation environment. 
+
+Run the server with `python server.py`. Then, use the provided Jupyter notebooks (`notebook_client_1.ipynb` and `notebook_client_2.ipynb`) to connect to the server and independently modify the simulation's state, such as changing the color of agents.
--- server.py
+++ server.py
@@ -3,23 +3,28 @@ from flask import Flask, request, jsonify
 # Toy example to see how to change the color of agents inside an environment
 class SimulationEnvironment:
     def __init__(self):
-        self.current_time = 0
         self.agent_color = 'red' 
 
-# Initialize the simulation environment and the server 
+# Initialize the simulation environment, the number of connections and the server 
 env = SimulationEnvironment()
+nb_connections = 0
 server = Flask(__name__)
 
 # Display a message when you connect to the server
 @server.route('/status', methods=['GET'])
 def connected():
-    return "Connected to the simulation server"
+    global nb_connections
+
+    # Increment the number of connections and return it 
+    nb_connections += 1
+    return f"Client number {nb_connections} connected to the simulation server"
 
 # Change the color of agent inside the simulation environment 
 @server.route('/change_agent_color', methods=['POST'])
 def change_agent_color():
     global env
 
+    # Change the color of agents 
     old_color = env.agent_color
     new_color = request.json.get('color')
     env.agent_color = new_color
